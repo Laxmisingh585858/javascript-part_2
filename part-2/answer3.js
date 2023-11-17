4b49f59f3a8790fd84e131a7bb720cc8abeb45e0
Laxmi Singh <146053155+Laxmisingh585858@users.noreply.github.com>
2023-11-17 20:25:13 +0530

    Add files via upload

---
+++ part-2/answer3.js
@@ -0,0 +1,7 @@
+program to check positive ,negetive and zero
+
+
+
+const num1=-25;
+const checknum1= (num1>=0 )?( num1==0 ? "zero":"positive"):" negative";
+console.log(checknum1);
---
+++ part-2/answer4.js
@@ -0,0 +1,3 @@
+const age=34;
+const vote= age>=18 ? " you can vote":" you cannot vote";
+console.log(vote);
